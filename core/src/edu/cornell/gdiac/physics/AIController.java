5e0c4fb7efc889e4340dca89acef2e27c05ff9a4
Haoxuan Zou <hz252@cornell.edu>
2023-03-03 21:50:43 -0500

    Update AIController.java

--- core/src/edu/cornell/gdiac/physics/AIController.java
+++ core/src/edu/cornell/gdiac/physics/AIController.java
@@ -2,11 +2,18 @@ package edu.cornell.gdiac.physics;
 
 public class AIController {
 
+    private int id;
+    private Board board;
+
     private static enum FSMState {
 
     }
     private static final int ATTACK_DIST = 4;
     public AIController(int id, Board board){
-
+        this.id = id;
+        this.board = board;
     }
+
+
+
 }
