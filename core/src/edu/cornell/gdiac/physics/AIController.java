79919af444de42d08feb7cece52aa12f816acb4d
Haoxuan Zou <hz252@cornell.edu>
2023-03-03 21:54:24 -0500

    Update AIController, add spec

--- core/src/edu/cornell/gdiac/physics/AIController.java
+++ core/src/edu/cornell/gdiac/physics/AIController.java
@@ -2,13 +2,22 @@ package edu.cornell.gdiac.physics;
 
 public class AIController {
 
+    private static final int ATTACK_DIST = 4;
+
+    /**The unique id specifying which AI this controller is assigned to*/
     private int id;
+
+    /**The game board to communicate map information, such as if there is a wall ahead*/
     private Board board;
 
+    /**
+     * The FSMState SHOULD control whether the AI is in wandering, chasing,
+     * or attacking mode.
+     * */
     private static enum FSMState {
 
     }
-    private static final int ATTACK_DIST = 4;
+
     public AIController(int id, Board board){
         this.id = id;
         this.board = board;
