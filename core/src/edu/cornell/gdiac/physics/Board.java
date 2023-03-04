6b2f282eba46ca3137a2e0260c95974e2e347427
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-03-03 21:45:00 -0500

    AI

--- core/src/edu/cornell/gdiac/physics/Board.java
+++ core/src/edu/cornell/gdiac/physics/Board.java
@@ -6,4 +6,13 @@ public class Board {
     public Board(int width, int height) {
         this.width = width;
         this.height = height;}
+
+    /**
+     * Add a list of points to the board
+     * @param points
+     */
+    public void Add(float[] points){
+
+    }
+
 }
