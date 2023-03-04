13741e21f7670e2ce8b6fee2de9a2b9b80f5ca3e
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-03-03 22:13:12 -0500

    AI

--- core/src/edu/cornell/gdiac/physics/Board.java
+++ core/src/edu/cornell/gdiac/physics/Board.java
@@ -1,18 +1,27 @@
 package edu.cornell.gdiac.physics;
 
+import java.util.ArrayList;
+
 public class Board {
     private int width;
     private int height;
+    private ArrayList<float[]> arrays;
     public Board(int width, int height) {
         this.width = width;
-        this.height = height;}
-
+        this.height = height;
+        this.arrays = new ArrayList<>();}
     /**
      * Add a list of points to the board
      * @param points
      */
     public void Add(float[] points){
-
+        arrays.add(points);
     }
 
+    /**
+     * If I can move to this point without pass the edge of a platform
+     */
+    public boolean CanGo(float x, float y){
+
+    }
 }
