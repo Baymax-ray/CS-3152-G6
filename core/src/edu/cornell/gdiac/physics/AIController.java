598e0e8aa716902355ac2f43136a31eca4230f9e
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-03-03 21:37:49 -0500

    AI

--- core/src/edu/cornell/gdiac/physics/AIController.java
+++ core/src/edu/cornell/gdiac/physics/AIController.java
@@ -2,6 +2,11 @@ package edu.cornell.gdiac.physics;
 
 public class AIController {
 
-    
+    private static enum FSMState {
 
+    }
+    private static final int ATTACK_DIST = 4;
+    public AIController(int id, Board board){
+
+    }
 }
---
+++ core/src/edu/cornell/gdiac/physics/Board.java
@@ -0,0 +1,9 @@
+package edu.cornell.gdiac.physics;
+
+public class Board {
+    private int width;
+    private int height;
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;}
+}
