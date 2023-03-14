c80db6ba91526aa2f9747fb885b56ea8c1047129
Haoxuan Zou <hz252@cornell.edu>
2023-03-13 20:01:15 -0400

    enemy basic implementation

--- Eudaemon/core/src/edu/cornell/gdiac/game/models/Enemy.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/models/Enemy.java
@@ -1,9 +1,14 @@
 package edu.cornell.gdiac.game.models;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.utils.JsonValue;
+import edu.cornell.gdiac.game.obstacle.CapsuleObstacle;
 
-public class Enemy {
+public class Enemy extends CapsuleObstacle implements ContactListener {
 
     //#region FINAL FIELDS
 
@@ -46,6 +51,27 @@ public class Enemy {
 
 
     public Enemy(JsonValue json) {
+        super();
         throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
     }
 }
