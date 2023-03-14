1c6fb2e4626bf1bd0f0f0c0037cd3d5ffd9d70f6
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-03-14 18:04:55 -0400

    GoombaAI

--- Eudaemon/core/src/edu/cornell/gdiac/game/GoombaAI.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/GoombaAI.java
@@ -13,9 +13,14 @@ public class GoombaAI extends AIController{
     private int ticks=0;
     private FSMState state;
     private EnemyAction move;
+    /**the amount of time to wait before back to wander state*/
+    private int WanderWait=0;
+    private float[] goal;
     private enum FSMState {
         /** The enemy just spawned */
         SPAWN,
+        /**The first frame the enemy starts to wander*/
+        TOWANDER,
         /** The enemy is patrolling around without a target */
         WANDER,
         /** The enemy has a target, but must get closer */
@@ -30,6 +35,22 @@ public class GoombaAI extends AIController{
     }
     public void setEnemyAction(EnumSet<EnemyAction> enemyAction){
         ticks=ticks+1;
+        // Process the FSM
+        changeStateIfApplicable();
+
+        // Pathfinding
+        markGoal();
+        MoveAlongPathToGoal();
+        switch(move){
+            case STAY:
+                break;
+            case MOVE_LEFT:
+                
+                break;
+            case MOVE_RIGHT:
+                break;
+        }
+
     }
     public boolean checkDetection(){
         float py=level.getPlayer().getY();
@@ -45,8 +66,13 @@ public class GoombaAI extends AIController{
                     return false;
                 }
             }
+            this.WanderWait=0;
             return true;
         }
+    }
+    public boolean canAttack(){
+        //ToDo
+        return false;
     }
     private void changeStateIfApplicable() {
         Random rand = new Random();
@@ -54,14 +80,27 @@ public class GoombaAI extends AIController{
         switch (state) {
             case SPAWN:
                 if (ticks>60){
-                    state=FSMState.WANDER;
+                    state=FSMState.TOWANDER;
                 }
+                break;
+            case TOWANDER:
+                state=FSMState.WANDER;
             case WANDER:
                 if (checkDetection()){
                     state=FSMState.CHASE;
                 }
+                break;
             case CHASE:
-                //TODO
+                if (canAttack()){
+                    state=FSMState.ATTACK;
+                }
+                else if (!checkDetection()){
+                    WanderWait++;
+                    if (WanderWait>10){
+                        WanderWait=0;
+                        state=FSMState.TOWANDER;
+                    }
+                }
                 break;
             case ATTACK:
                 //TODO
@@ -69,7 +108,71 @@ public class GoombaAI extends AIController{
         }
 
     }
-    public void markGoal(){}
-    public EnemyAction getMoveAlongPathToGoal(){}
+    public void markGoal(){
+        float ex=enemy.getX();
+        float ey=enemy.getY();
 
-}
+        switch (state) {
+            case SPAWN:
+                goal[0]=ex;
+                goal[1]=ey;
+            break;
+            case TOWANDER:
+                boolean c=true;
+                goal[1]=ey;
+                while(c){
+                    int a=1;
+                    if(level.isAirAt(ex+a,ey-1)){
+                        goal[0]=ex+a;
+                        c=false;
+                    } else if (level.isAirAt(ex-a,ey-1)) {
+                        goal[0]=ex-a;
+                        c=false;
+                    }
+                    a++;
+                }
+                break;
+            case WANDER:
+                if (goal[0]>ex && level.isAirAt(ex+1,ey-1)){
+                    boolean cl=true;
+                    while(cl){
+                        int a=1;
+                        if (level.isAirAt(ex-a,ey-1)) {
+                            goal[0]=ex-a;
+                            cl=false;
+                        }
+                        a++;
+                    }
+                }
+                else if(goal[0]<ex && level.isAirAt(ex-1,ey-1)){
+                    boolean cr=true;
+                    while(cr){
+                        int a=1;
+                        if (level.isAirAt(ex+a,ey-1)) {
+                            goal[0]=ex-a;
+                            cr=false;
+                        }
+                        a++;
+                    }
+                }
+                break;
+            case CHASE:
+                goal[0]=level.getPlayer().getX();
+                break;
+    }}
+    public void MoveAlongPathToGoal(){
+        float ex=enemy.getX();
+        float ey=enemy.getY();
+
+        if(ex<=goal[0]+0.1||ex>=goal[0]-0.1){this.move=EnemyAction.STAY;}
+        else if (ex<goal[0]){
+            this.move=EnemyAction.MOVE_RIGHT;
+            if(level.isAirAt(ex+1,ey-1)){this.move=EnemyAction.STAY;}
+        }
+        else if (ex>goal[0]){
+            this.move=EnemyAction.MOVE_LEFT;
+            if(level.isAirAt(ex-1,ey-1)){this.move=EnemyAction.STAY;}
+        }
+        }
+    }
+
