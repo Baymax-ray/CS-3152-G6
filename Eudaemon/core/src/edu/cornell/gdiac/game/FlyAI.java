7bd61f86b9c53cd128fc353719888abf02544aef
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-03-14 22:05:49 -0400

    some work in FlyAI and delete two unused assests

--- Eudaemon/core/src/edu/cornell/gdiac/game/FlyAI.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/FlyAI.java
@@ -9,6 +9,20 @@ import java.util.EnumSet;
 public class FlyAI extends AIController{
     private Enemy enemy;
     private Level level;
+    private int ticks=0;
+    private FSMState state;
+    private enum FSMState {
+        /** The enemy just spawned */
+        SPAWN,
+        /**The first frame the enemy starts to wander*/
+        TOWANDER,
+        /** The enemy is patrolling around without a target */
+        WANDER,
+        /** The enemy has a target, but must get closer */
+        CHASE,
+        /** The enemy has a target and is attacking it */
+        ATTACK
+    }
     public FlyAI(int ii, Level level) {
         super(ii,level);
     }
