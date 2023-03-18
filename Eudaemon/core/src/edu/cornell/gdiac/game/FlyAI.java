8d56163389456f74eca0fc20a41db219a024ec14
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-03-18 18:53:55 -0400

    slight change in constant about enemy

--- Eudaemon/core/src/edu/cornell/gdiac/game/FlyAI.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/FlyAI.java
@@ -61,6 +61,19 @@ public class FlyAI extends AIController{
         }
     }
 
+    private boolean sameCell(){
+        float py=level.getPlayer().getY();
+        float px=level.getPlayer().getX();
+        float ey=enemy.getY();
+        float ex=enemy.getX();
+        if (Math.abs(py-ey)<1&&Math.abs(px-ex)<1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     private boolean canAttack(){
         //ToDo
         return false;
@@ -81,6 +94,9 @@ public class FlyAI extends AIController{
                 if (canAttack()){
                     state= FSMState.ATTACK;
                 }
+                else if(sameCell()){
+                    state= FSMState.CHASE_close;
+                }
                 else if (!checkDetection()){
                     WanderWait++;
                     if (WanderWait>maxWait){
@@ -89,6 +105,10 @@ public class FlyAI extends AIController{
                     }
                 }
                 break;
+            case CHASE_close:
+                if(sameCell()){
+                    state=FSMState.CHASE;
+                }
             case ATTACK:
                 //TODO
                 break;
@@ -118,22 +138,41 @@ public class FlyAI extends AIController{
                 goal[1]=ny;
                 break;
             case CHASE:
+                // only need to fly to the same cell
+                goal[0]=(float) Math.round(level.getPlayer().getX());
+                goal[1]=(float) Math.round(level.getPlayer().getY());
+                break;
+            case CHASE_close:
                 goal[0]=level.getPlayer().getX();
                 goal[1]=level.getPlayer().getY();
                 break;
-        }}
-    private void MoveAlongPathToGoal(){
-        float ex=enemy.getX();
-        float ey=enemy.getY();
 
-        if(ex<=goal[0]+0.1||ex>=goal[0]-0.1){this.move=EnemyAction.STAY;}
-        else if (ex<goal[0]){
-            this.move=EnemyAction.MOVE_RIGHT;
-            if(level.isAirAt(ex+1,ey-1)){this.move=EnemyAction.STAY;}
-        }
-        else if (ex>goal[0]){
-            this.move=EnemyAction.MOVE_LEFT;
-            if(level.isAirAt(ex-1,ey-1)){this.move=EnemyAction.STAY;}
+        }}
+    private void MoveAlongPathToGoal() {
+        float ex = enemy.getX();
+        float ey = enemy.getY();
+
+        switch (state) {
+            case CHASE_close:
+                float dx=goal[0]-ex;
+                float dy=goal[1]-ey;
+                if (Math.abs(dx)>Math.abs(dy)){
+                    if (dx>0){
+                        this.move=EnemyAction.FLY_RIGHT;
+                    }else{
+                        this.move=EnemyAction.FLY_LEFT;
+                    }
+                }else{
+                    if (dy>0){
+                        this.move=EnemyAction.FLY_UP;
+                    }
+                    else{
+                        this.move=EnemyAction.FLY_DOWN;
+                    }
+                }
+                break;
+            default:
+            break;
         }
     }
 }
