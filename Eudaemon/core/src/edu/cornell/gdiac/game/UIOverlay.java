7d0c90c843d6765b28a7546e9adb85873d26142d
Varsha Iyer <vvi2@cornell.edu>
2023-03-26 22:25:57 -0400

    added a settings button asset - does not work yet

--- Eudaemon/core/src/edu/cornell/gdiac/game/UIOverlay.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/UIOverlay.java
@@ -19,15 +19,19 @@ public class UIOverlay {
     /** Texture for outline of bar/hearts*/
     private final Texture uiOutline;
 
+    private final Texture settingsButton;
+
     public UIOverlay(JsonValue json, AssetDirectory assets){
         this.heartRegion = assets.getEntry(json.getString("heart"), Texture.class);
         this.filledSpiritBar = assets.getEntry(json.getString("filled"), Texture.class);
         this.uiOutline = assets.getEntry(json.getString("uioutline"), Texture.class);
+        this.settingsButton = assets.getEntry(json.getString("settings"), Texture.class);
     }
 
     public void draw(GameCanvas canvas, float playerHearts, float playerSpirit) {
         float xPos = 69;
         canvas.draw(uiOutline, Color.WHITE, 0, canvas.getHeight() - 62, 221, 52);
+        canvas.draw(settingsButton, Color.WHITE, canvas.getWidth()-72, canvas.getHeight()-62, 51, 52);
         for(int i=0; i < playerHearts; i++){
             canvas.draw(heartRegion, Color.WHITE, xPos,canvas.getHeight() - 32, 23, 20);
             xPos += 31.7F;
