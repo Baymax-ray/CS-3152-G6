58104cc26296ac0b58b866703b9df2ad1f0aad91
Haoxuan Zou <hz252@cornell.edu>
2023-04-10 02:32:03 -0400

    try to cancel the collision between flying enemies but failed

--- Eudaemon/core/src/edu/cornell/gdiac/game/CollisionController.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/CollisionController.java
@@ -35,47 +35,31 @@ public class CollisionController implements ContactListener {
         Object bd2 = body2.getUserData();
 
         try {
-            if ((body1.getUserData().toString().contains("Tile") && fix2.getUserData().equals(
+            if ((bd1.toString().contains("Tile") && fix2.getUserData().equals(
                     level.getPlayer().getSensorName())) ||
-                    (body2.getUserData().toString().contains("Tile") && fix1.getUserData().equals(
+                    (bd2.toString().contains("Tile") && fix1.getUserData().equals(
                             level.getPlayer().getSensorName()))) {
                 level.getPlayer().setGrounded(true);
-                sensorFixtures.add(level.getPlayer() == body1.getUserData() ? fix2 : fix1);
+                sensorFixtures.add(level.getPlayer() == bd1 ? fix2 : fix1);
             }
         } catch (Exception e) { }
 
+
         if(bd1 instanceof Enemy || bd1 instanceof Player){
-            //System.out.println("YES, hi");
-
+            //System.out.println("YES1, hi");
+            //System.out.println(((Enemy) bd1).getType().equals("FlyGuardian"));
             Fixture fixture1 = fix1;
-
             Filter filter1 = fixture1.getFilterData();
-
-            //currently, we'll always go to the else statement. I don't know why.
-            //One possible reason: the collision of the flying enemies are just not considered.
-            if(bd1 instanceof Enemy && (((Enemy) bd1).getType() == "FlyGuardian" || ((Enemy) bd1).getType() == "Fly") ){
-                System.out.println("fly, hi");
-                filter1.groupIndex = 1;
-            }else{
-                filter1.groupIndex = -1;
-            }
+            filter1.groupIndex = -1;
             fixture1.setFilterData(filter1);
         }else if (bd2 instanceof Enemy || bd2 instanceof Player) {
-            //System.out.println("YES, hi");
+            //System.out.println("YES2, hi");
+            //System.out.println(((Enemy) bd2).getType() == "FlyGuardian");
             Fixture fixture1 = fix2;
-
             Filter filter1 = fixture1.getFilterData();
-
-
-            if(bd2 instanceof Enemy && (((Enemy) bd2).getType() == "FlyGuardian" || ((Enemy) bd2).getType() == "Fly") ){
-                System.out.println("fly, hi");
-                filter1.groupIndex = 1;
-            }else{
-                filter1.groupIndex = -1;
-            }
+            filter1.groupIndex = -1;
             fixture1.setFilterData(filter1);
         }
-
 
 
     }
@@ -92,12 +76,12 @@ public class CollisionController implements ContactListener {
         Object bd2 = body2.getUserData();
 
         try {
-            if ((body1.getUserData().toString().contains("Tile") && fix2.getUserData().equals(
+            if ((bd1.toString().contains("Tile") && fix2.getUserData().equals(
                     level.getPlayer().getSensorName())) ||
-                    (body2.getUserData().toString().contains("Tile") && fix1.getUserData().equals(
+                    (bd2.toString().contains("Tile") && fix1.getUserData().equals(
                             level.getPlayer().getSensorName()))) {
 
-                sensorFixtures.removeValue(level.getPlayer() == body1.getUserData() ? fix2 : fix1, true);
+                sensorFixtures.removeValue(level.getPlayer() == bd1 ? fix2 : fix1, true);
                 if (sensorFixtures.size == 0) {
                     level.getPlayer().setGrounded(false);
                 }
@@ -116,30 +100,28 @@ public class CollisionController implements ContactListener {
 
         Object bd1 = body1.getUserData();
         Object bd2 = body2.getUserData();
-        //below is an attempt to cancel the collision between flying enemies, but failed.
-//        if(bd1 instanceof Enemy && bd2 instanceof Enemy){
-//            System.out.println("YES, hi");
-//
-//            if(
-//                    (((Enemy) bd1).getType() == "FlyGuardian" || ((Enemy) bd1).getType() == "Fly") &&
-//                            (((Enemy) bd2).getType() == "FlyGuardian" || ((Enemy) bd2).getType() == "Fly")
-//            ){
-//                System.out.println("flying , hi");
-//                Fixture fixture1 = fix1;
-//
-//                Filter filter1 = fixture1.getFilterData();
-//
-//                filter1.groupIndex = 1;
-//                fixture1.setFilterData(filter1);
-//                Fixture fixture2 = fix2;
-//
-//                Filter filter2 = fixture2.getFilterData();
-//
-//                filter2.groupIndex = 1;
-//                fixture2.setFilterData(filter2);
-//            }
-//
-//        }
+        //the following if statement cannot be realized. idk why.
+        if(bd1 instanceof Enemy && bd2 instanceof Enemy){
+
+            System.out.println("flying1, hi!");
+            System.out.println(((Enemy) bd1).getType());
+            System.out.println(((Enemy) bd2).getType());
+            if(
+                    (((Enemy) bd1).getType().equals( "FlyGuardian") || ((Enemy) bd1).getType().equals( "Fly")) &&
+                            (((Enemy) bd2).getType().equals( "FlyGuardian") || ((Enemy) bd2).getType().equals( "Fly"))){
+                System.out.println("flying2, hi!");
+                Fixture fixture1 = fix1;
+
+                Filter filter1 = fixture1.getFilterData();
+                Fixture fixture2 = fix2;
+
+                Filter filter2 = fixture1.getFilterData();
+                filter1.groupIndex = 1;
+                filter2.groupIndex = 1;
+                fixture1.setFilterData(filter1);
+                fixture2.setFilterData(filter2);
+            }
+        }
 
         //the following code is trying to remove the collision between player and enemy when player is dashing.
         //this is not used because now the player should not be colliding with enemies anyways.
@@ -166,14 +148,14 @@ public class CollisionController implements ContactListener {
         //Intended to solve the situation when player is stuck on wall by a flying enemy sticking to it
         //and cannot escape
         //BUT FAILED.
-        Fixture fix1 = contact.getFixtureA();
-        Fixture fix2 = contact.getFixtureB();
-
-        Body body1 = fix1.getBody();
-        Body body2 = fix2.getBody();
-
-        Object bd1 = body1.getUserData();
-        Object bd2 = body2.getUserData();
+//        Fixture fix1 = contact.getFixtureA();
+//        Fixture fix2 = contact.getFixtureB();
+//
+//        Body body1 = fix1.getBody();
+//        Body body2 = fix2.getBody();
+//
+//        Object bd1 = body1.getUserData();
+//        Object bd2 = body2.getUserData();
 
 //      same as above: dash collision, not used.
 //        if(bd1 instanceof Player && !((Player) bd1).isDashing()){
