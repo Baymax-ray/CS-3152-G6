f72b98dfa1cf063cb806f304ad0243dffbe4e955
Raymond <raymond@markable.ai>
2023-05-04 22:05:03 -0400

    updated assets & TutorialArea bug fix

--- Eudaemon/core/src/edu/cornell/gdiac/game/CollisionController.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/CollisionController.java
@@ -349,13 +349,15 @@ public class CollisionController implements ContactListener {
         }
 
         if (bd1 instanceof Player && bd2 instanceof Billboard
-                || bd2 instanceof Player && bd1 instanceof Billboard) {
+                || bd2 instanceof Player && bd1 instanceof Billboard
+                && (!level.getPlayer().getSpiritSensorName().equals(fd1) && !level.getPlayer().getSpiritSensorName().equals(fd2))) {
             Billboard billboard = (Billboard) (bd1 instanceof Billboard? bd1 : bd2);
             billboard.setDisplay(false);
         }
 
-        if (bd1 instanceof Player && bd2 instanceof TutorialArea
-                || bd2 instanceof Player && bd1 instanceof TutorialArea) {
+        if ((bd1 instanceof Player && bd2 instanceof TutorialArea
+                || bd2 instanceof Player && bd1 instanceof TutorialArea)
+                && (!level.getPlayer().getSpiritSensorName().equals(fd1) && !level.getPlayer().getSpiritSensorName().equals(fd2))) {
             TutorialArea tutorialArea = (TutorialArea) (bd1 instanceof TutorialArea? bd1 : bd2);
             tutorialArea.setDisplay(false);
         }
