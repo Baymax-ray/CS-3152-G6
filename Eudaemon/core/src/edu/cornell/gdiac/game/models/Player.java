13c4d02e08b6633ce3704abb76f8605f3c1edd6f
neelin1 <zaneneelinprojects@gmail.com>
2023-05-19 23:01:28 -0400

    - initial spirit

--- Eudaemon/core/src/edu/cornell/gdiac/game/models/Player.java
+++ Eudaemon/core/src/edu/cornell/gdiac/game/models/Player.java
@@ -1430,7 +1430,7 @@ public class Player extends CapsuleObstacle {
         this.isChiyo = false;
 
         this.hearts = initialHearts;
-        this.spirit = 10;
+        this.spirit = initialSpirit;
 
         this.isDashing = false;
         this.isJumping = false;
